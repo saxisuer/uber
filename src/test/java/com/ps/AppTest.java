c05645af3ce40d5596317033328bfcdd6499a9f5
saxisuer <killenblood@gmail.com>
2016-01-20 00:55:28 +0800

    提交测试环境

---
+++ src/test/java/com/ps/AppTest.java
@@ -0,0 +1,20 @@
+package com.ps;
+
+import org.junit.Test;
+
+/**
+ * @Project uber
+ * @Package com.ps
+ * @Description //TODO
+ * @Date 16/1/20
+ * @USER saxisuer
+ * @COMPANY ENMOTECH
+ */
+public class AppTest {
+
+
+    @Test
+    public void test() {
+        System.out.println("test");
+    }
+}
